7778cfbfc56185c8a1965cfe61618155321dda40
Uday Goyat <udaygoyat45@gmail.com>
2020-01-31 03:18:23 +0000

    added the signing form

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,12 +1,22 @@
 from flask import request, redirect, render_template, url_for, flash
 from flaskapp import db, app
 from flaskapp.forms import RegistrationForm
+from flaskapp.models import Email
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
 
-@app.route("/sign")
+@app.route("/sign", methods=['POST', 'GET'])
 def sign():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        newEmail = Email(email=form.email.data)
+        db.session.add(newEmail)
+        db.session.commit()
+
+        flash("Thanks for registering")
+        return render_template("home.html")
+    
     return render_template("sign.html")
